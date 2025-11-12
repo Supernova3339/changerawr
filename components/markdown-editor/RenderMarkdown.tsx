838b289043e4cdf4cc1196b6d7fad779fc029e27
SuperDev <supernova@supers0ft.us>
2025-11-11 19:41:17 -0600

    add the fixes

--- components/markdown-editor/RenderMarkdown.tsx
+++ components/markdown-editor/RenderMarkdown.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import React from 'react';
-import { renderMarkdown } from '@/lib/services/core/markdown/useCustomExtensions';
+import React, {useMemo} from 'react';
+import {renderMarkdown} from '@/lib/services/core/markdown/useCustomExtensions';
+import {useDebounce} from "use-debounce";
 
 interface RenderMarkdownProps {
     children: string;
@@ -10,16 +11,21 @@ interface RenderMarkdownProps {
 }
 
 export const RenderMarkdown: React.FC<RenderMarkdownProps> = ({
-                                                                  children,
+                                                                  children = '',
                                                                   className = ''
                                                               }) => {
     // Use our new markdown renderer
-    const html = renderMarkdown(children);
+    const [debouncedContent] = useDebounce(children || '', 300);
+
+    const renderedHtml = useMemo(() => {
+        const content = debouncedContent || '';
+        return renderMarkdown(content);
+    }, [debouncedContent]);
 
     return (
         <div
             className={`prose max-w-none prose-img:my-4 prose-headings:mt-6 prose-headings:mb-4 prose-p:mb-4 prose-pre:my-4 prose-blockquote:my-4 ${className}`}
-            dangerouslySetInnerHTML={{ __html: html }}
+            dangerouslySetInnerHTML={{__html: renderedHtml}}
             suppressHydrationWarning
         />
     );
